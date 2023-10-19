b6e08fd2d070053732320fed4a0c556a9c8443b7
0xPr0f <0xprofoz@gmail.com>
2023-10-19 20:25:54 +0100

    added bot

---
+++ packages/bot/bot.js
@@ -0,0 +1,551 @@
+//require("dotenv").config();
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+import { run } from "@grammyjs/runner";
+import "dotenv/config";
+import {
+  createRandomWallet,
+  createSafeWallet,
+  fundSafeWalletWithEth,
+  createSafeTransactionSendETH,
+  importSafeWallet,
+  sendUserEth,
+  userEthBalance,
+} from "./safeinteraction.js";
+const { hydrate } = require("@grammyjs/hydrate");
+const {
+  conversations,
+  createConversation,
+} = require("@grammyjs/conversations");
+const { Bot, session, GrammyError, HttpError } = require("grammy");
+const { Menu } = require("@grammyjs/menu");
+const {
+  bold,
+  fmt,
+  hydrateReply,
+  italic,
+  link,
+  code,
+  spoiler,
+} = require("@grammyjs/parse-mode");
+
+const bot = new Bot(process.env.BOT_TOKEN);
+const NETWORK_SCAN_ADDRESS = "https://goerli.etherscan.io/address/";
+const NETWORK_SCAN_TX = "https://goerli.etherscan.io/tx/";
+
+/// using sessions to persist values && creating initial sessions data
+function createInitialSessionData() {
+  return {
+    log: 1,
+    existing_chat: false,
+    generated_wallet_address: process.env.WALLET_ADDRESS,
+    generated_wallet_privatekey: "",
+    generated_wallet_mnemonic: process.env.PRIVATE_MNEMONIC,
+    safe_wallets_array: [],
+    safe_wallet: "",
+    has_safe_wallet: false,
+    edit_able_message_id: 0,
+  };
+}
+bot.use(session({ initial: createInitialSessionData }));
+bot.use(hydrateReply);
+bot.use(conversations());
+bot.use(hydrate());
+async function ask_for_reciever_amount_input(conversation, ctx) {
+  /*if (!ctx.session.existing_chat)
+    return ctx.replyFmt(fmt`Error processing request`); */
+  const balance = await userEthBalance(ctx.session.generated_wallet_mnemonic);
+  console.log("bot", balance);
+
+  await ctx.replyFmt(
+    fmt`You are transfering from your Eth balance
+ETH balance : ${code(balance)} ETH`
+  );
+  ctx.replyFmt(fmt`Please input reciever address`);
+
+  // TODO: code the conversation
+  const reciever_address = await conversation.waitFor("message:text");
+  if (reciever_address.message?.text === "/cancel") {
+    await ctx.replyFmt(fmt`Cancelled conversation`);
+    return;
+  }
+  if (!/^0x[a-fA-F0-9]{40}$/gm.test(reciever_address.message?.text.trim()))
+    return ctx.replyFmt(fmt`Invalid Address format`);
+
+  console.log(reciever_address.message, "raw rct");
+  console.log(reciever_address.message.text, "rct");
+  console.log(
+    reciever_address.message.text.toString().trim(),
+    "string and trim"
+  );
+  console.log(reciever_address.message.text.trim(), "trim");
+  ctx.replyFmt(fmt`Please input amount`);
+  const amount_input = await conversation.waitFor("message:text");
+  if (amount_input.message?.text === "/cancel") {
+    await ctx.replyFmt(fmt`Cancelled conversation`);
+    return;
+  }
+  if (!/^(0|[1-9]\d*)(\.\d+)?$/.test(amount_input.message?.text.trim()))
+    return ctx.replyFmt(fmt`Invalid Amount format`);
+  const verify = await ctx.replyFmt(
+    fmt`Proccessing transaction..., This process will take ~ 5minutes`
+  );
+  try {
+    const _sendUserEthtx = await sendUserEth(
+      reciever_address.message.text,
+      amount_input.message.text,
+      ctx.session.generated_wallet_mnemonic
+    );
+    console.log(_sendUserEthtx, "tx");
+    const fmtString = fmt`Successfully Transferred ${code(
+      amount_input.message.text.trim()
+    )} ETH to ${code(reciever_address.message.text.trim())} ${link(
+      "↗",
+      "" + NETWORK_SCAN_TX + _sendUserEthtx + ""
+    )}`;
+    await verify.editText(fmtString.toString(), {
+      entities: fmtString.entities,
+      disable_web_page_preview: true,
+    });
+
+    return;
+  } catch (e) {
+    console.log(e);
+    console.log(e.Error);
+    if (e === 701) {
+      verify.editText("Invalid amount format");
+      return;
+    }
+    if (e === 702) {
+      verify.editText("Cannot import. This safe does not belong to this user.");
+      return;
+    }
+    if (e === 703) {
+      verify.editText("Invalid address format");
+      return;
+    }
+    if (e === 705) {
+      verify.editText("Insufficient balance for this transaction");
+      return;
+    }
+    return;
+  }
+}
+bot.use(createConversation(ask_for_reciever_amount_input));
+async function ask_for_safe_transfer_eth_input(conversation, ctx) {
+  /*if (!ctx.session.existing_chat)
+    return ctx.replyFmt(fmt`Error processing request`); */
+  const balance = await userEthBalance(ctx.session.generated_wallet_mnemonic);
+  console.log("bot", balance);
+
+  await ctx.replyFmt(
+    fmt`You are transfering from your safe Eth balance
+ETH balance : ${code(balance)} ETH`
+  );
+  ctx.replyFmt(fmt`Please input reciever address`);
+
+  // TODO: code the conversation
+  const reciever_address = await conversation.waitFor("message:text");
+  if (reciever_address.message?.text === "/cancel") {
+    await ctx.replyFmt(fmt`Cancelled conversation`);
+    return;
+  }
+  if (!/^0x[a-fA-F0-9]{40}$/gm.test(reciever_address.message?.text.trim()))
+    return ctx.replyFmt(fmt`Invalid Address format`);
+
+  console.log(reciever_address.message, "raw rct");
+  console.log(reciever_address.message.text, "rct");
+  console.log(
+    reciever_address.message.text.toString().trim(),
+    "string and trim"
+  );
+  console.log(reciever_address.message.text.trim(), "trim");
+  ctx.replyFmt(fmt`Please input amount`);
+  const amount_input = await conversation.waitFor("message:text");
+  if (amount_input.message?.text === "/cancel") {
+    await ctx.replyFmt(fmt`Cancelled conversation`);
+    return;
+  }
+  if (!/^(0|[1-9]\d*)(\.\d+)?$/.test(amount_input.message?.text.trim()))
+    return ctx.replyFmt(fmt`Invalid Amount format`);
+  const verify = await ctx.replyFmt(
+    fmt`Proccessing transaction..., This process will take ~ 5minutes`
+  );
+  try {
+    const _sendUserEthtx = await createSafeTransactionSendETH(
+      reciever_address.message.text,
+      amount_input.message.text,
+      ctx.session.generated_wallet_mnemonic,
+      ctx.session.safe_wallet
+    );
+    console.log(_sendUserEthtx, "tx");
+    const fmtString = fmt`Successfully Transferred ${code(
+      amount_input.message.text.trim()
+    )} ETH to ${code(reciever_address.message.text.trim())} ${link(
+      "↗",
+      "" + NETWORK_SCAN_TX + _sendUserEthtx + ""
+    )}`;
+    await verify.editText(fmtString.toString(), {
+      entities: fmtString.entities,
+      disable_web_page_preview: true,
+    });
+
+    return;
+  } catch (e) {
+    console.log(e);
+    console.log(e.Error);
+    if (e === 701) {
+      verify.editText("Invalid amount format");
+      return;
+    }
+    if (e === 702) {
+      verify.editText("Cannot import. This safe does not belong to this user.");
+      return;
+    }
+    if (e === 703) {
+      verify.editText("Invalid address format");
+      return;
+    }
+    if (e === 705) {
+      verify.editText("Insufficient balance for this transaction");
+      return;
+    }
+    return;
+  }
+}
+bot.use(createConversation(ask_for_safe_transfer_eth_input));
+
+//bot.use(createConversation(ask_for_input_for_safeimport));
+const intro_menu = new Menu("my-intro-menu-identifier")
+  .text("Fund wallet", (ctx) => {
+    ctx.session.log++;
+    ctx.replyFmt(
+      fmt`${fmt`Click the address to copy it.`}
+${bold(
+  fmt`▰ ${link(
+    "Wallet",
+    NETWORK_SCAN_ADDRESS + ctx.session.generated_wallet_address
+  )} ▰ `
+)}
+${code(ctx.session.generated_wallet_address)}
+${fmt`Send ethereum to this address to pay for gas`}
+      `,
+      { disable_web_page_preview: true }
+    );
+  })
+  .row()
+  .text("Create Safe Wallet", async (ctx) => {
+    const verify = await ctx.replyFmt(
+      fmt`Please wait while we setup you Safe Wallet, This process will take ~ 5minutes ...`
+    );
+    try {
+      const safeAccountWallet = await createSafeWallet(
+        ctx.session.generated_wallet_mnemonic
+      );
+      console.log(safeAccountWallet);
+      ctx.session.safe_wallet = safeAccountWallet;
+      ctx.session.has_safe_wallet = true;
+      verify.editText("Safe Wallet successfully created: " + safeAccountWallet);
+      // Menu_refresh(ctx);
+    } catch (error) {
+      verify.editText(
+        `Cannot create safe wallet. Please ensure you have enough ETH to cover the gas fee.`
+      );
+      console.log(error);
+    }
+  })
+
+  /* .text("Import Safe Wallet", async (ctx) => {
+    await ctx.conversation.enter("ask_for_input_for_safeimport");
+  })
+  */
+  .submenu("Manage Safe", "my-safe-identifier")
+  .row()
+
+  .text("Help", (ctx) => ctx.replyFmt("help"))
+  .submenu("⚙️ Settings", "my-settings-identifier");
+
+const refresh_menu = new Menu("my-refresh-menu-identifier")
+  .text(
+    "Fund Safe wallet",
+    (ctx) => {
+      console.log("test");
+      /*
+    ctx.replyFmt(
+      fmt`${fmt`Click the address to copy it.`}
+${bold(
+  fmt`▰ ${link(
+    "Wallet",
+    NETWORK_SCAN_ADDRESS + ctx.session.generated_wallet_address
+  )} ▰ `
+)}
+${code(ctx.session.generated_wallet_address)}
+${fmt`Send ethereum to this address to pay for gas`}
+      `*/ //,
+    },
+    { disable_web_page_preview: true }
+  )
+  .row()
+  .text("Transfer Safe Eth ", async (ctx) => {})
+  .text("Transfer Safe Token ", async (ctx) => {})
+  .row()
+  .text("Help", (ctx) => ctx.replyFmt("help"));
+// .submenu("⚙️ Settings", "my-settings-identifier");
+bot.use(refresh_menu);
+
+async function ask_for_input_for_safeimport(conversation, ctx) {
+  ctx.replyFmt(fmt`Please input the address of the safe account`);
+  // TODO: code the conversation
+  const { message } = await conversation.waitFor("message:text");
+  console.log(message?.text);
+  if (message?.text === "/cancel") {
+    await ctx.replyFmt(fmt`Cancelled conversation`);
+    return;
+  }
+  if (!/^0x[a-fA-F0-9]{40}$/gm.test(message?.text))
+    return ctx.replyFmt(fmt`Invalid Address format`);
+  const verify = await ctx.replyFmt(
+    fmt`Please wait while we verify..., This process will take ~ 5minutes`
+  );
+  try {
+    ctx.session.safe_wallet = message?.text;
+    ctx.session.has_safe_wallet = true;
+    console.log(ctx.session.safe_wallet, "bool check 320");
+    console.log(ctx.session.has_safe_wallet, "bool check 321");
+    console.log("started the stuff");
+    const ImportSafeWallet = await importSafeWallet(
+      ctx.session.generated_wallet_mnemonic,
+      message.text
+    );
+    console.log();
+    verify.editText(`Successfully Imported: ${message.text}`);
+    // Menu_refresh(ctx);
+    return;
+  } catch (e) {
+    console.log(e);
+    console.log(e.Error);
+    if (e === 701) {
+      verify.editText("Cannot import. Invalid address.");
+      return;
+    }
+    if (e === 702) {
+      verify.editText("Cannot import. This safe does not belong to this user.");
+      return;
+    }
+    if (e === 704) {
+      verify.editText("Cannot import. Safe does not exist on this network.");
+      return;
+    }
+    return;
+  }
+}
+
+bot.use(createConversation(ask_for_input_for_safeimport));
+
+const settings_menu = new Menu("my-settings-identifier")
+  .back("⬅ Menu")
+  .text("Close", (ctx) => {
+    ctx.replyFmt(
+      fmt`${bold(fmt`═══ Your Wallets ═══`)}
+${bold(
+  fmt`▰ ${link(
+    "Wallet",
+    NETWORK_SCAN_ADDRESS + ctx.session.generated_wallet_address
+  )} ▰`
+)}
+${code(ctx.session.generated_wallet_address)}
+      `,
+      {
+        reply_markup: intro_menu,
+        disable_web_page_preview: true,
+      }
+    );
+    ctx.deleteMessage();
+  })
+  .row()
+  .text("Transfer Eth", async (ctx) => {
+    await ctx.conversation.enter("ask_for_reciever_amount_input");
+  })
+  .text("Transfer Token ", (ctx) => ctx.replyFmt("transfer_tokens"))
+  .row()
+  .text("Import Safe Wallet", async (ctx) => {
+    await ctx.conversation.enter("ask_for_input_for_safeimport");
+  })
+  .row()
+  .text("View Mnemonic", (ctx) =>
+    ctx.replyFmt(
+      fmt`${bold("Keep your mnemonic safe and do not share it with anyone.")}
+${spoiler(ctx.session.generated_wallet_mnemonic)}
+`
+    )
+  )
+  .text("Regenerate Account", (ctx) => ctx.replyFmt("regenerate_account"));
+
+const safe_menu = new Menu("my-safe-identifier")
+  .back("⬅ Menu")
+  .row()
+  .text("Fund safe wallet ETH", async (ctx) => {})
+  .row()
+  .text("Transfer Safe Eth", async (ctx) => {
+    if (!ctx.session.has_safe_wallet) {
+      ctx.replyFmt("User currently have no Created or Imported Safe");
+      return;
+    }
+    const transferSafeETH = createSafeTransactionSendETH();
+    ctx.replyFmt("eth_transfer_tokens");
+    try {
+      //  const sendingEth = sendUserEth();
+    } catch (error) {}
+  })
+  .text("Transfer Safe Token ", async (ctx) => {
+    if (!ctx.session.has_safe_wallet)
+      ctx.replyFmt("User currently have no Created or Imported Safe");
+    ctx.replyFmt("transfer_tokens");
+  })
+  .row()
+  .text("Import Safe Wallet", async (ctx) => {
+    await ctx.conversation.enter("ask_for_input_for_safeimport");
+  });
+/*
+  .text("Close", (ctx) => {
+    ctx.replyFmt(
+      fmt`${bold(fmt`═══ Your Wallets ═══`)}
+${bold(
+  fmt`▰ ${link(
+    "Wallet",
+    NETWORK_SCAN_ADDRESS + ctx.session.generated_wallet_address
+  )} ▰`
+)}
+${code(ctx.session.generated_wallet_address)}
+      `,
+      {
+        reply_markup: intro_menu,
+        disable_web_page_preview: true,
+      }
+    );
+    ctx.deleteMessage();
+  })
+*/
+
+/*
+  .text("View Mnemonic", (ctx) =>
+    ctx.replyFmt(
+      fmt`${bold("Keep your mnemonic safe and do not share it with anyone.")}
+${spoiler(ctx.session.generated_wallet_mnemonic)}
+`
+    )
+  )
+  .text("Regenerate Account", (ctx) => ctx.replyFmt("regenerate_account"));
+*/
+bot.use(intro_menu);
+intro_menu.register(safe_menu);
+intro_menu.register(settings_menu);
+// Handle the /start command.
+// Handle the /start command.
+bot.command("start", (ctx) => {
+  Start(ctx);
+});
+bot.command("start", (ctx) => {
+  Start(ctx);
+});
+
+bot.command("help", (ctx) => ctx.replyFmt("hello, you need help"));
+
+// Handle other messages.
+bot.on("message", (ctx) => {
+  ctx.replyFmt("Got another message!");
+});
+
+bot.use(refresh_menu);
+
+const Menu_refresh = (ctx) => {
+  ctx.replyFmt(
+    fmt`${
+      !ctx.session.existing_chat
+        ? fmt`Welcome to safebot.
+You are now registered and have been assigned a new wallet.
+Create or Import a safe wallet to transact safely on the blockchian with OTP authentication.`
+        : ""
+    }
+
+${bold(fmt`═══ Your Wallets ═══`)}
+${bold(
+  fmt`▰ ${link(
+    "Wallet",
+    NETWORK_SCAN_ADDRESS + ctx.session.generated_wallet_address
+  )} ▰ `
+)}
+${code(ctx.session.generated_wallet_address)}
+
+${
+  ctx.session.has_safe_wallet
+    ? fmt`${bold(fmt`═══ Safe Wallet ═══`)}
+${bold(
+  fmt`▰ ${link("Wallet", NETWORK_SCAN_ADDRESS + ctx.session.safe_wallet)} ▰ `
+)}
+${code(ctx.session.safe_wallet)}`
+    : ""
+}
+
+`,
+    { reply_markup: refresh_menu, disable_web_page_preview: true }
+  );
+  ctx.session.existing_chat = true;
+};
+
+const Start = (ctx) => {
+  /* will comment out when ready for testing 
+  if (!ctx.session.existing_chat) {
+    const user_wallet = createRandomWallet();
+    ctx.session.generated_wallet_mnemonic = user_wallet.mnemonic.phrase;
+    ctx.session.generated_wallet_address = user_wallet.address;
+    console.log(user_wallet);
+  }
+*/
+  console.log("bool for safe wallet", ctx.session.has_safe_wallet);
+  console.log("bool for safe wallet 504", ctx.session.safe_wallet);
+  ctx.replyFmt(
+    fmt`${
+      !ctx.session.existing_chat
+        ? fmt`Welcome to safebot.
+You are now registered and have been assigned a new wallet.
+Create or Import a safe wallet to transact safely on the blockchian with OTP authentication.`
+        : ""
+    }
+
+${bold(fmt`═══ Your Wallets ═══`)}
+${bold(
+  fmt`▰ ${link(
+    "Wallet ↗",
+    NETWORK_SCAN_ADDRESS + ctx.session.generated_wallet_address
+  )} ▰ `
+)}
+${code(ctx.session.generated_wallet_address)}
+${
+  ctx.session.has_safe_wallet
+    ? fmt`${bold(fmt`═══ Safe Wallet ═══`)}
+${bold(
+  fmt`▰ ${link("Wallet", NETWORK_SCAN_ADDRESS + ctx.session.safe_wallet)} ▰ `
+)}
+${code(ctx.session.safe_wallet)}`
+    : "Hello saucy"
+}`,
+    { reply_markup: intro_menu, disable_web_page_preview: true }
+  );
+  ctx.session.existing_chat = true;
+};
+
+bot.catch((err) => {
+  const ctx = err.ctx;
+  console.error(`Error while handling update ${ctx.update.update_id}:`);
+  const e = err.error;
+  if (e instanceof GrammyError) {
+    console.error("Error in request:", e.description);
+  } else if (e instanceof HttpError) {
+    console.error("Could not contact Telegram:", e);
+  } else {
+    console.error("Unknown error:", e);
+  }
+});
+
+run(bot);
---
+++ packages/bot/safeinteraction.js
@@ -0,0 +1,307 @@
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const { ethers } = require("ethers");
+const {
+  Safe,
+  EthersAdapter,
+  SafeFactory,
+  SafeAccountConfig,
+} = require("@safe-global/protocol-kit");
+import SafeApiKit from "@safe-global/api-kit";
+
+///state RPC, provider and info for the interactions
+/// we will be using goerli for now
+// polygon mumbai rpc url = "https://polygon-mumbai.gateway.tenderly.co"
+// we will also be using goerli txServiceUrl
+const RPC_URL = "https://ethereum-goerli.publicnode.com";
+const txServiceUrl = "https://safe-transaction-goerli.safe.global";
+const provider = new ethers.providers.JsonRpcProvider(RPC_URL);
+const { TEST_PRIVATE_KEY } = process.env;
+const blockNumber = 5;
+const createRandomWallet = () => {
+  const user_details = ethers.Wallet.createRandom();
+  return user_details;
+};
+
+/// function to create safe wallet
+const createSafeWallet = async (generated_wallet_mnemonic) => {
+  const _walletMnemonicInstance = ethers.Wallet.fromMnemonic(
+    generated_wallet_mnemonic
+  );
+
+  console.log(_walletMnemonicInstance.privateKey);
+  const walletMnemonicInstance = new ethers.Wallet(
+    _walletMnemonicInstance.privateKey,
+    provider
+  );
+  //const login_user_signer = walletMnemonicInstance.connect(provider);
+  const login_user_signer = walletMnemonicInstance;
+  console.log(await login_user_signer.getAddress());
+  const safeAccountConfig = {
+    owners: [await login_user_signer.getAddress()],
+    threshold: 1,
+  };
+  const ethAdapterSafeWalletCreation = new EthersAdapter({
+    ethers,
+    signerOrProvider: login_user_signer,
+  });
+  const safeFactory = await SafeFactory.create({
+    ethAdapter: ethAdapterSafeWalletCreation,
+  });
+  /* const txOptions = {
+    gas: 2100000,
+    gasPrice: 8000000000,
+  }; */
+  //const txOptions = { gasLimit: 2100000 };
+  const txOptions = {
+    gasLimit: ethers.utils.hexlify(21000000),
+    gasPrice: ethers.utils.parseUnits("15", "gwei"),
+  };
+  const timestamp = (await provider.getBlock("latest")).timestamp;
+
+  console.log(timestamp);
+  const user_safe_account = await safeFactory.deploySafe({
+    safeAccountConfig,
+    saltNonce: timestamp,
+  });
+
+  const safeAddress = await user_safe_account.getAddress();
+
+  console.log("Your Safe has been deployed:");
+  console.log(`https://goerli.etherscan.io/address/${safeAddress}`);
+  console.log(`https://app.safe.global/gor:${safeAddress}`);
+
+  return safeAddress;
+};
+
+/// function to create safe wallet
+/* Erorr codes
+ * 701 - input (safe) address does not match a valid eth address
+ * 702 - input (safe) address does not belong/no valid signer to the user
+ * 703 - amount is not valid input
+ * 704 - input safe address is not a safe on that chain
+ * 705 - insufficient balance
+ */
+
+const importSafeWallet = async (
+  generated_wallet_mnemonic,
+  safe_wallet_address
+) => {
+  const walletMnemonicInstance = ethers.Wallet.fromMnemonic(
+    generated_wallet_mnemonic
+  );
+  const login_user_signer = walletMnemonicInstance.connect(provider);
+  const ethAdapterSafeWalletCreation = new EthersAdapter({
+    ethers,
+    signerOrProvider: login_user_signer,
+  });
+  const userAddress = await login_user_signer.getAddress();
+  const safeAccountConfig = {
+    owners: [await login_user_signer.getAddress()],
+    threshold: 1,
+  };
+
+  const safeService = new SafeApiKit.default({
+    txServiceUrl: "https://safe-transaction-goerli.safe.global",
+    ethAdapter: ethAdapterSafeWalletCreation,
+  });
+  console.log("started the stuff 1");
+  if (/^0x[a-fA-F0-9]{40}$/gm.test(safe_wallet_address.trim())) {
+    // check if the address belongs to the user
+    console.log("started the stuff 2");
+    var safeInfo;
+    try {
+      safeInfo = await safeService.getSafeInfo(safe_wallet_address);
+    } catch (error) {
+      throw 704;
+    }
+    console.log(safeInfo);
+    console.log("started the stuff 3");
+    console.log(safeInfo);
+    for (var i = 0; safeInfo.owners.length >= i; i++) {
+      if (safeInfo.owners[i] === userAddress) {
+        console.log("started the stuff 4");
+        console.log(true);
+        return true;
+      }
+    }
+    throw 702;
+  } else {
+    throw 701;
+  }
+};
+
+const fundSafeWalletWithEth = async (
+  generated_wallet_mnemonic,
+  safeWalletAddress,
+  amountInEth
+) => {
+  const _walletMnemonicInstance = ethers.Wallet.fromMnemonic(
+    generated_wallet_mnemonic
+  );
+
+  console.log(_walletMnemonicInstance.privateKey);
+  const walletMnemonicInstance = new ethers.Wallet(
+    _walletMnemonicInstance.privateKey,
+    provider
+  );
+  //const login_user_signer = walletMnemonicInstance.connect(provider);
+  const login_user_signer = walletMnemonicInstance;
+  console.log(await login_user_signer.getAddress());
+
+  const safeFundAmount = ethers.utils
+    .parseUnits(amountInEth.toString(), "ether")
+    .toHexString();
+
+  const transactionParameters = {
+    to: safeWalletAddress,
+    value: safeFundAmount,
+  };
+  try {
+    const tx = await login_user_signer.sendTransaction(transactionParameters);
+    return tx;
+  } catch (error) {
+    console.log(error);
+  }
+
+  console.log("Fundraising.");
+  console.log(`Deposit Transaction: https://goerli.etherscan.io/tx/${tx.hash}`);
+};
+
+const createSafeTransactionSendETH = async (
+  destination,
+  amountInEth,
+  generated_wallet_mnemonic,
+  safe_wallet_address
+) => {
+  if (!/^(0|[1-9]\d*)(\.\d+)?$/.test(amountInEth.trim())) throw 703;
+  const amount = ethers.utils
+    .parseUnits(amountInEth.toString(), "ether")
+    .toString();
+  const _walletMnemonicInstance = ethers.Wallet.fromMnemonic(
+    generated_wallet_mnemonic
+  );
+
+  //console.log(_walletMnemonicInstance.privateKey);
+  const walletMnemonicInstance = new ethers.Wallet(
+    _walletMnemonicInstance.privateKey,
+    provider
+  );
+  const login_user_signer = walletMnemonicInstance;
+  const safeTransactionData = {
+    to: destination,
+    data: "0x",
+    value: amount,
+  };
+  const Injection = new EthersAdapter({
+    ethers,
+    signerOrProvider: login_user_signer,
+  });
+  const SafeInjection = await Safe.create({
+    ethAdapter: Injection,
+    safe_wallet_address,
+  });
+  // Create a Safe transaction with the provided parameters
+  const safeTransaction = await SafeInjection.createTransaction({
+    safeTransactionData,
+  });
+  console.log(safeTransaction);
+  return safeTransaction;
+};
+/*
+exports.safeinteractions = {
+  createSafeWallet,
+  createRandomWallet,
+  fundSafeWalletWithEth,
+  createSafeTransactionETH,
+};
+*/
+/*
+exports.createRandomWallet = createRandomWallet;
+exports.createSafeWallet = createSafeWallet;
+exports.importSafeWallet = importSafeWallet;
+exports.fundSafeWalletWithEth = fundSafeWalletWithEth;
+exports.createSafeTransactionETH = createSafeTransactionETH;
+*/
+
+const sendUserEth = async (
+  destination,
+  amountInEth,
+  generated_wallet_mnemonic
+) => {
+  if (!/^(0|[1-9]\d*)(\.\d+)?$/.test(amountInEth.trim())) throw 701;
+  if (!/^0x[a-fA-F0-9]{40}$/gm.test(destination.trim())) throw 703;
+
+  const _walletMnemonicInstance = ethers.Wallet.fromMnemonic(
+    generated_wallet_mnemonic
+  );
+
+  //console.log(_walletMnemonicInstance.privateKey);
+  const walletMnemonicInstance = new ethers.Wallet(
+    _walletMnemonicInstance.privateKey,
+    provider
+  );
+
+  // Create a transaction object
+  let tx = {
+    to: destination,
+    // Convert currency unit from ether to wei
+    value: ethers.utils.parseEther(amountInEth),
+  };
+  const balance = await userEthBalance(generated_wallet_mnemonic);
+  if (ethers.utils.parseEther(amountInEth) >= balance) throw 705;
+
+  // Send a transaction
+  return walletMnemonicInstance.sendTransaction(tx).then((txObj) => {
+    console.log("txHash", txObj.hash);
+    return txObj.hash;
+  });
+};
+
+const userEthBalance = async (generated_wallet_mnemonic, address) => {
+  if (generated_wallet_mnemonic) {
+    const _walletMnemonicInstance = ethers.Wallet.fromMnemonic(
+      generated_wallet_mnemonic
+    );
+
+    //console.log(_walletMnemonicInstance.privateKey);
+    const walletMnemonicInstance = new ethers.Wallet(
+      _walletMnemonicInstance.privateKey,
+      provider
+    );
+    try {
+      return provider
+        .getBalance(
+          await walletMnemonicInstance.getAddress(),
+          await provider.getBlock(blockNumber).timestamp
+        )
+        .then((balance) => {
+          console.log(balance.toString() / 10 ** 18);
+          return balance.toString() / Math.pow(10, 18);
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  if (address) {
+    try {
+      return provider
+        .getBalance(address, await provider.getBlock(blockNumber).timestamp)
+        .then((balance) => {
+          console.log(balance.toString() / 10 ** 18);
+          return balance.toString() / Math.pow(10, 18);
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+};
+export {
+  createRandomWallet,
+  createSafeWallet,
+  importSafeWallet,
+  fundSafeWalletWithEth,
+  createSafeTransactionSendETH,
+  sendUserEth,
+  userEthBalance,
+};
